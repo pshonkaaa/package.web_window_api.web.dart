8c2694166392bfd2b30edd3d628097c8af715018
TrueCollaboration <pshonka.dev@gmail.com>
2024-05-22 22:39:25 +0200

    backup

--- lib/src/external/classes/promise.dart
+++ lib/src/external/classes/promise.dart
@@ -1,7 +1,14 @@
+import 'dart:js_util';
+
 import 'package:js/js.dart' as js;
 
 @js.JS()
 class Promise<T> {
   external Promise(void executor(void resolve(T result), Function reject));
   external Promise then(void onFulfilled(T result), [Function onRejected]);
+}
+
+extension PromiseEx<T> on Promise<T> {
+  Future<T> asFuture()
+    => promiseToFuture<T>(this);
 }
